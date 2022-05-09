d3e5662375fcdef443c521d3e6779b441cbf70bd
Prabhu Teja Pamula <ajetdream@yahoo.com>
2022-05-09 20:04:13 +0530

    a

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm;
+  constructor(private formBuilder:FormBuilder,route:Router) { }
+
+  ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+      email :['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
+  login(){
+     
+  }
+}
